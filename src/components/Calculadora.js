7870e01980b7d3d593747caeb8d7db57df251263
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-03-18 01:02:27 -0300

    Teclado chumbado no componente

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -7,15 +7,39 @@ import Display from './Display'
 export default class Calculadora extends React.Component {  
 
     state = {
-        displayValue: '0'
+        displayValue: '24'
+    }
+
+    addDigit = (n) => {
+        this.setState({ displayValue: `${n}` })
+    }
+
+    clearMemory = () => {
+        this.setState({ displayValue: '0' })
+    }
+
+    setOperation = () => {
+        
     }
     
-    criarBotoes = (arr, ...estilo) => {
+    criarBotoesNumericos = (arr, ...estilo) => {
         const estiloValor = estilo
-        
+
         return arr.map((el, idx) => {
-            return <Button key={`${idx}`} label={el} estilo={estiloValor}></Button>
+            return <Button key={`${idx}`} label={el} 
+            estilo={estiloValor} OnClick={() => this.addDigit(el)}></Button>
         })
+        
+    }
+
+    criarBotoesOperadores = (arr, ...estilo) => {
+        const estiloValor = estilo
+
+        return arr.map((el, idx) => {
+            return <Button key={`${idx}`} label={el} 
+            estilo={estiloValor} OnClick={() => this.clearMemory()}></Button>
+        })
+        
     }
     
     gerarNumerosTeclado = () => {
@@ -25,14 +49,14 @@ export default class Calculadora extends React.Component {
             numsTeclado.push(index)
         }
 
-        return this.criarBotoes(numsTeclado) 
+        return this.criarBotoesNumericos(numsTeclado) 
     }
 
     gerarOperadoresTeclado = () => {
         const opTeclado = ['/','*','-','+','=']
 
-        return this.criarBotoes(opTeclado, 'operationButton')
-
+        return this.criarBotoesOperadores(opTeclado, 'operationButton')
+        
     }
 
     render () {
@@ -40,10 +64,23 @@ export default class Calculadora extends React.Component {
             <>
                 <Display value={this.state.displayValue}/>
                 <View style={styles.buttons}>
-                    <Button label="AC"/>
-                    {this.gerarNumerosTeclado()}
-                    <Button label="."/>
-                    {this.gerarOperadoresTeclado()}
+                    <Button label="AC" estilo={['buttonTriple']} OnClick={this.clearMemory}/>
+                    <Button label="/" estilo={['operationButton']} OnClick={() => this.setOperation('/')}/>
+                    <Button label="7" OnClick={() => this.addDigit(7)}/>
+                    <Button label="8" OnClick={() => this.addDigit(8)}/>
+                    <Button label="9" OnClick={() => this.addDigit(9)}/>
+                    <Button label="x" estilo={['operationButton']} OnClick={() => this.setOperation('*')}/>
+                    <Button label="4" OnClick={() => this.addDigit(4)}/>
+                    <Button label="5" OnClick={() => this.addDigit(5)}/>
+                    <Button label="6" OnClick={() => this.addDigit(6)}/>
+                    <Button label="-" estilo={['operationButton']} OnClick={() => this.setOperation('-')}/>
+                    <Button label="1" OnClick={() => this.addDigit(1)}/>
+                    <Button label="2" OnClick={() => this.addDigit(2)}/>
+                    <Button label="3" OnClick={() => this.addDigit(3)}/>
+                    <Button label="+" estilo={['operationButton']} OnClick={() => this.setOperation('-')}/>
+                    <Button label="0" estilo={['buttonDouble']} OnClick={() => this.addDigit(0)}/>
+                    <Button label="." OnClick={() => this.addDigit('.')}/>
+                    <Button label="=" estilo={['operationButton']} OnClick={() => this.setOperation('=')}/>
                 </View>
             </>
         )
