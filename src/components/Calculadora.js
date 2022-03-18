6eec40d4af739cf082c3a6d6adf8d0c286f7a03d
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-03-18 01:37:10 -0300

    Função gerarLinha para números

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native'
 import Button from './Button'
 import Display from './Display'
 
-export default class Calculadora extends React.Component {  
+export default class Calculadora extends React.Component { 
 
     state = {
         displayValue: '24'
@@ -59,6 +59,10 @@ export default class Calculadora extends React.Component {
         
     }
 
+    gerarLinha = (...n) => {
+        return this.criarBotoesNumericos(n)
+    }
+
     render () {
         return (
             <>
@@ -66,17 +70,11 @@ export default class Calculadora extends React.Component {
                 <View style={styles.buttons}>
                     <Button label="AC" estilo={['buttonTriple']} OnClick={this.clearMemory}/>
                     <Button label="/" estilo={['operationButton']} OnClick={() => this.setOperation('/')}/>
-                    <Button label="7" OnClick={() => this.addDigit(7)}/>
-                    <Button label="8" OnClick={() => this.addDigit(8)}/>
-                    <Button label="9" OnClick={() => this.addDigit(9)}/>
+                    {this.gerarLinha(7,8,9)}
                     <Button label="x" estilo={['operationButton']} OnClick={() => this.setOperation('*')}/>
-                    <Button label="4" OnClick={() => this.addDigit(4)}/>
-                    <Button label="5" OnClick={() => this.addDigit(5)}/>
-                    <Button label="6" OnClick={() => this.addDigit(6)}/>
+                    {this.gerarLinha(4,5,6)}
                     <Button label="-" estilo={['operationButton']} OnClick={() => this.setOperation('-')}/>
-                    <Button label="1" OnClick={() => this.addDigit(1)}/>
-                    <Button label="2" OnClick={() => this.addDigit(2)}/>
-                    <Button label="3" OnClick={() => this.addDigit(3)}/>
+                    {this.gerarLinha(1,2,3)}
                     <Button label="+" estilo={['operationButton']} OnClick={() => this.setOperation('-')}/>
                     <Button label="0" estilo={['buttonDouble']} OnClick={() => this.addDigit(0)}/>
                     <Button label="." OnClick={() => this.addDigit('.')}/>
